d7c657f34ef1521ba7ff97d9c2d61600a049a000
TomasRuizM <tomasruiz@paucasesnovescifp.cat>
2023-03-30 00:38:23 +0200

    Clase registroUsuario completada

--- src/Registro/registroUsuario.java
+++ src/Registro/registroUsuario.java
@@ -1,4 +1,29 @@
 package Registro;
 
-public class registroUsuario {
+import java.util.Scanner;
+import java.util.Random;
+
+import static Registro.ValidarCampos.*;
+
+/**
+ * @author Tomas
+ */
+
+public class RegistroUsuario {
+    static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
+    static String[] nombres = {"Juan", "María", "Pedro", "Lucía", "Luis", "Ana", "Diego", "Laura", "Carlos", "Elena"};
+
+    public static void main(String[] args) {
+        String nombre = compruebaNombre();
+        String email = compruebaEmail();
+        String password = compruebaPassword();
+        String codigo = generaCodigoSeguridad();
+
+        System.out.println("El registro se ha realizado con éxito.");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Email: " + email);
+        System.out.println("Password: " + password);
+        System.out.println("Código de seguridad: " + codigo);
+    }
 }
