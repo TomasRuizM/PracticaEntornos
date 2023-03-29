5544543dfe9c063739cd5ca19e56961a5d4e77b3
ruben <rubenespinar@paucasesnocescifp.cat>
2023-03-30 00:45:41 +0200

    Metodos metodos password y codigo de seguridad

--- src/Registro/validarCampos.java
+++ src/Registro/validarCampos.java
@@ -1,4 +1,37 @@
 package Registro;
 
 public class validarCampos {
+
+    public static String compruebaPassword() {
+        String password;
+        boolean esValido;
+        do {
+            System.out.print("Introduce tu password: ");
+            password = scanner.nextLine();
+            esValido = true;
+            if (!password.matches("^[A-Z][a-zA-Z0-9@#_-]{6}[0-9]{2}$")) {
+                System.out.println("El password no cumple con los requisitos.");
+                esValido = false;
+            }
+        } while (!esValido);
+        return password;
+    }
+
+    public static String generaCodigoSeguridad() {
+        String codigo = "";
+        String caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890@#_-";
+        for (int i = 0; i < 8; i++) {
+            codigo += caracteres.charAt(random.nextInt(caracteres.length()));
+        }
+        System.out.println("El código de seguridad es: " + codigo);
+        String codigoUsuario;
+        do {
+            System.out.print("Introduce el código de seguridad: ");
+            codigoUsuario = scanner.nextLine();
+            if (!codigoUsuario.equals(codigo)) {
+                System.out.println("El código de seguridad no coincide.");
+            }
+        } while (!codigoUsuario.equals(codigo));
+        return codigo;
+    }
 }
